4ba48ee7c3d9bd16a6c24794f65af162cd6b2eb6
soytupadrrre <victorluque314@gmail.com>
2022-10-20 20:06:57 +0200

    POK-4

---
+++ pokemon_repo.py
@@ -0,0 +1,6 @@
+from pokemon import Pokemon
+from pydantic_mongo import AbstractRepository
+
+class PokemonRepository(AbstractRepository[Pokemon]):
+    class Meta:
+        collection_name = "pokemons"
