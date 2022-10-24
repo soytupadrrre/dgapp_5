16a15e6f4ef4f064389f2d9f8501800e52bd39a9
soytupadrrre <victorluque314@gmail.com>
2022-10-24 19:42:59 +0200

    moved

--- app/pokemon_repo.py
+++
@@ -1,6 +0,0 @@
-from app.pokemon import Pokemon
-from pydantic_mongo import AbstractRepository
-
-class PokemonRepository(AbstractRepository[Pokemon]):
-    class Meta:
-        collection_name = "pokemons"
